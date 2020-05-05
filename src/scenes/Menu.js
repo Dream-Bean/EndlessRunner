cdd070c8d67f262fa69f3ea250013b6bdc53b819
ajshaham <ashaham@ucsc.edu>
2020-05-04 18:34:10 -0700

    menuscene edit

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -5,7 +5,7 @@ class Menu extends Phaser.Scene {
 
     // load menu assets
     preload() {
-        this.load.image('gamelogo', './assets/gamelogo.png');
+        this.load.spritesheet('gamelogo', './assets/gamelogo_empty.png', { frameWidth: 173, frameHeight: 66, startFrame: 0, endFrame: 1 });
         this.load.image('botbg', './assets/street.png');
         this.load.image('topbg', './assets/finalsw.png');
     }
@@ -14,12 +14,22 @@ class Menu extends Phaser.Scene {
         // Place tile sprite
         this.wpTop = this.add.tileSprite(0, 0, 632, 70, 'topbg').setScale(4, 4).setOrigin(0, 0);
         this.wpBot = this.add.tileSprite(0, 283, 316, 108, 'botbg').setScale(4.1, 4).setOrigin(0, 0);
-        this.logo = this.add.tileSprite(game.config.width / 2, game.config.height / 2 - 150, 179, 67, 'gamelogo').setScale(2, 2).setOrigin(0.5);
         // Borders
         this.add.rectangle(0, 0, 1280, 5, 0x000000).setOrigin(0, 0); // left
         this.add.rectangle(0, 715, 1280, 5, 0x000000).setOrigin(0, 0); // bottom
         this.add.rectangle(0, 0, 5, 720, 0x000000).setOrigin(0, 0); // top
         this.add.rectangle(1275, 0, 5, 720, 0x000000).setOrigin(0, 0); // right  
+
+        this.anims.create({
+            key: 'gamelogo',
+            frames: this.anims.generateFrameNumbers('gamelogo', { start: 0, end: 1, first: 0 }),
+            frameRate: 5,
+            setScale: 1,
+            repeat: -1,
+        });
+        console.log("w0t");
+        this.logo = new Player(this, game.config.width / 2, game.config.height / 2 - 50, 'gamelogo').setScale(2.5, 2.5).setOrigin(0.5); // bootleg way to do it...
+        this.logo.anims.play('gamelogo')
 
         // text display
         let menuConfig = {
@@ -37,7 +47,7 @@ class Menu extends Phaser.Scene {
         // show menu text
         let centerX = game.config.width / 2;
         let centerY = game.config.height / 2;
-        this.add.text(centerX, centerY + 110, 'Press F To Play', menuConfig).setOrigin(0.5);
+        this.add.text(centerX, centerY + 110, 'Press F To Play', menuConfig).setScale(2).setOrigin(0.5);
         this.add.text(centerX, centerY + 160, 'Press T For Tutorial', menuConfig).setOrigin(0.5);
 
         // define keys
